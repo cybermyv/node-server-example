5eef2c0d55e4d43db823eb54c65324c5c44d2663
Yuriy Mironov <git>
2017-08-16 16:50:47 +0400

    [*] добавил раут

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 //git remote add origin https://github.com/cybermyv/node-server-example.git
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import dbEngine from './dbengine';
 
 
 const app = express();
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + './../Client/'));
 
 
 app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', async(req, res) => {
-    res.end('default route ...');
-    //  res.sendfile('./views/index.html');
+app.get('/', async(req, res) => {
+    //  res.end('default route ...');
+    res.sendfile('index.html');
 });
+
+app.get('/list', async(req, res, next) => {
+    try {
+        const list = await dbEngine.all('SELECT * FROM AROMA');
+        console.log(list);
+
+        res.send(list);
+    } catch (err) {
+        next(err)
+    }
+});
+
 
 app.listen(8080, err => {
     if (err) throw err;
